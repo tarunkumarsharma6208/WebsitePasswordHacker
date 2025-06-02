be6b9273dbe97e1f703c37be8322e92e80048775
Tarun kumar <tarun.kumar@tech58.in>
2025-06-02 15:17:10 +0530

    requirements

--- passwordfinder/settings.py
+++ passwordfinder/settings.py
@@ -74,23 +74,13 @@ WSGI_APPLICATION = 'passwordfinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pw',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
